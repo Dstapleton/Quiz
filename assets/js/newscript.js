68f5672a95cef230e51e6277822959d24a451d3c
@davidS <dstapletongo@gmail.com>
2021-01-08 18:32:02 -0500

    questions added and footer display

--- assets/js/newscript.js
+++ assets/js/newscript.js
@@ -67,8 +67,14 @@ section1.appendChild(generalDisplay);
 
 // display text
 var gdisplayh2 = document.createElement("h2");
-gdisplayh2.innerHTML = "Welcome";
+gdisplayh2.innerHTML = "Coding Quiz Challenge";
 generalDisplay.appendChild(gdisplayh2);
+
+// display text
+var pdisplay = document.createElement("p");
+pdisplay.id = "intro";
+pdisplay.innerHTML = "Try to answer the folling code-related questions within the time limit.Keep in mind that the incorect answer will penalize your score/time by ten seconds";
+generalDisplay.appendChild(pdisplay);
 
 // quiz display
 var quizDisplay = document.createElement("div");
@@ -83,20 +89,52 @@ quizButton.className = "qiuzBtn";
 quizButton.innerText = "Start Quiz";
 quizDisplay.appendChild(quizButton);
 
+// --- footer section
+
+// display element
+var answerDisplay = document.createElement("div");
+answerDisplay.className = "answerdisplay";
+footer.appendChild(answerDisplay);
+
+// display text wrong or right using active
+var answerTxt = document.createElement("h2");
+answerTxt.innerHTML = " ";
+answerDisplay.appendChild(answerTxt);
+
 
 
 // questions
 var questions = [
-    {question:"Commonly used data Do Not Include",
-     A:"1.strings",
-     B:"2.booleans",
-     C:"3.alerts",
-     D:"4.numbers",
-     answer:"C"},
-    {question:"Question 2", A:"one", B:"Mew", C:"care", D:"four", answer:"D"},
-    {question:"Question 3", A:"one", B:"two", C:"happy", D:"four", answer:"B"},
-    {question:"Question 4", A:"one", B:"Hope", C:"three", D:"More", answer:"A"},
-    {question:"Question 5", A:"one", B:"Hope", C:"three", D:"More", answer:"A"}
+    {question:"Commonly used data types DO NOT Include",
+        A:"1.strings",
+        B:"2.booleans",
+        C:"3.alerts",
+        D:"4.numbers",
+        answer:"C"},
+    {question:"String values must be enclosed within __ when being used",
+        A:"1.commas",
+        B:"2.curly brackets",
+        C:"3.quotations",
+        D:"4.parenthesis",
+        answer:"C"},
+    {question:"The condition in an if / else stament is enclosed with __.",
+        A:"1.quotes",
+        B:"2.curly brackets",
+        C:"3.parenthesis",
+        D:"4.squar brackets",
+        answer:"C"},
+    {question:"Arrays in JavaScript can be used to store __.",
+        A:"1.numbers and strings",
+        B:"2.other arrays",
+        C:"3.booleans",
+        D:"4.all of the above",
+        answer:"D"},
+    {question:"A very useful tool, used during development, debuging, and for printing content to the debuger is?",
+         A:"1.JavaScript",
+         B:"2.terminal/bash",
+         C:"3.for loop",
+         D:"4.consle.log",
+         answer:"D"}
 ];
 
 // score tracker
@@ -110,7 +148,7 @@ var score = [
 var questionKey = ['A','B','C','D'];
 var x = "";
 var z = 0;
-var quizEnd = false;
+var  quizEnd = false;
 
 // create buttons with question options
 var creatButtons = function (index) {
@@ -145,7 +183,7 @@ var creatQuestion = function (index) {
     
 }
 // quiz timer
-var quizTimer = function (time, count) {
+var quizTimer = function (time) {
     var min = time * 10;
     var quiz__Time = document.getElementById("quiz__timer");
     quiz__Time.innerText = "Time:" + " " + time;
@@ -156,14 +194,13 @@ var quizTimer = function (time, count) {
             endQuiz("Time UP");
             clearInterval(scoreTimer);
         }
-        else if (!count){
+        else if (!quizEnd){
             quiz__Time.innerText = "Time:" + " " + time;
             time--;
         }
-        else {
+        else {;
             clearInterval(scoreTimer);
         }
-        
     },min);
 }
 
@@ -190,22 +227,79 @@ var quizTimer = function (time, count) {
 var checkAnswer = function (event) {
     var quizAnswer = event.target;
     var quizCheck = questions;
+    var quickTime = 3;
     quizAnswer.disabled = "true";
     // get the indicatesd correct answer
      if (quizCheck[z].answer === quizAnswer.value){
         window.z++;
-        clearOptions("btn");
-        creatQuestion(z)
-        listener(checkAnswer,".btn");
         incrementPoints();
+
+        // paus for 3 secounds
+        var correctTimer = setInterval(function(){
+            clearOptions("btn");
+            creatQuestion(z)
+            listener(checkAnswer,".btn");
+            displayRightAnswer(quickTime);
+
+            quickTime--
+
+            //reset quick time
+            if(quickTime===0){
+                clearInterval(correctTimer);
+                quickTime = 3;
+               // addTime(4);
+            }
+        },300);
+        
      }
+     
      else{
-         decrementPoints();
+        displayWorngAnswer(quickTime);
+        decrementPoints();
+         //deductTime(1);
      }
 }
 
+// let the player know the answer was correct
+var displayRightAnswer = function(timer) {
+    if (timer === 1){
+        answerTxt.innerText = " ";
+    }
+    else{
+        answerTxt.innerText = "right";
+    }
+   
+}
 
+// let the player know the answer was wrong
+var displayWorngAnswer = function(timer) {
+    if (timer === 1){
+        answerTxt.innerText = " ";
+    }
+    else{
+        answerTxt.innerText = "wrong";
+    }
+   
+}
+
+// add time to current time
+var addTime = function (amount){
+    var currentTime = quizTimer.time;
+    currentTime += amount;
+}
+
+// dedect time from current time
+var deductTime = function (amount){
+    var deducttime = 2;
+    var quiz__Time = document.getElementById("quiz__timer");
+    deducttime += amount;
+    quiz__Time.innerText = "Time: " + deducttime;
+}
+
+// quiz start-up
 var startQuiz = function() {
+    //remove intro paragraph
+    pdisplay.style.visibility = "hidden";
     // hide start button
     quizButton.style.visibility = "hidden";
 
@@ -214,14 +308,17 @@ var startQuiz = function() {
     
     // generate the first question
     creatQuestion(z);
-    quizTimer(60,endQuiz);
+    quizTimer(60);
     listener(checkAnswer,".btn");
 }
 
 var endQuiz = function(elemet){
     var quiz__Time = document.getElementById("quiz__timer");
-    window.endQuiz = true;
+    window.quizEnd = true;
     quiz__Time.innerText = "Time:" + " " + elemet;
+   if (elemet === "Time UP"){
+       clearOptions("btn")
+   }
 }
 
 function incrementPoints(){
